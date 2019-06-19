0e99cce213445a0cc9d02b23430496df0bd39b17
Demian Katz <demian.katz@villanova.edu>
2019-06-19 10:07:43 -0400

    Add resume capabilities; improve comments.

---
+++ CHANGELOG.md
@@ -0,0 +1,29 @@
+# Changelog
+
+All notable changes to this project will be documented in this file, in reverse chronological order by release.
+
+## 2.0 - 2019-06-19
+
+### Added
+
+- State save on exception, with --resume option.
+
+### Changed
+
+- Constructor parameters for Crawler class have changed to support new functionality.
+
+### Deprecated
+
+- Nothing.
+
+### Removed
+
+Nothing.
+
+### Fixed
+
+- Nothing.
+
+## 1.0 - 2016-08-01
+
+Initial release.
--- Murpoint.php
+++ Murpoint.php
@@ -27,32 +27,111 @@ use EasyRdf\Graph;
 
 require 'vendor/autoload.php';
 
-$urlParts = isset($argv[1]) ? parse_url($argv[1]) : [];
-$outFile = isset($argv[2]) ? fopen($argv[2], 'w') : false;
-if (!isset($urlParts['host']) || empty($urlParts['host'])) {
-    die("First parameter must be valid starting URL.\n");
-}
-if (!$outFile) {
-    die("Second parameter must be writable file for output.\n");
+// Special case: resuming from dump file:
+try {
+    if ($argc > 0 && $argv[1] == '--resume') {
+        if (!isset($argv[2]) || !file_exists($argv[2])) {
+            die("Cannot open resume file.\n");
+        }
+        $crawler = Crawler::fromState($argv[2]);
+    } else {
+        // Standard case: starting new crawl
+        $urlParts = isset($argv[1]) ? parse_url($argv[1]) : [];
+        $outFile = isset($argv[2]) ? $argv[2] : false;
+        if (!isset($urlParts['host']) || empty($urlParts['host'])) {
+            die("First parameter must be valid starting URL.\n");
+        }
+        if (!$outFile) {
+            die("Second parameter must be writable file for output.\n");
+        }
+        $crawler = new Crawler($urlParts['host'], $outFile);
+        $crawler->enqueue($argv[1]);
+    }
+} catch (\Exception $e) {
+    die($e->getMessage() . "\n");
 }
 
-$crawler = new Crawler($urlParts['host'], $outFile);
-$crawler->enqueue($argv[1]);
-$crawler->run();
+try {
+    $crawler->run();
+} catch (\Exception $e) {
+    echo "Fatal error: " . $e->getMessage() . "\n";
+    echo "Dumping state...\n";
+    $dumpFile = $outFile . '.state';
+    $crawler->saveState($dumpFile);
+    echo "You can attempt to resume later with {$argv[0]} --resume $dumpFile\n";
+}
 
+/**
+ * Main crawler class to harvest RDF.
+ */
 class Crawler
 {
+    /**
+     * File handle for output.
+     *
+     * @var resource
+     */
     protected $handle;
-    protected $host;
-    protected $queue = [];
-    protected $visited = [];
 
-    public function __construct($host, $handle)
+    /**
+     * Host to harvest.
+     *
+     * @var string
+     */
+    protected $host;
+
+    /**
+     * Name of output file.
+     *
+     * @var string
+     */
+    protected $outFile;
+
+    /**
+     * Queue of pending URLs to fetch.
+     *
+     * @var string[]
+     */
+    protected $queue;
+
+    /**
+     * List of previously-visited URLs.
+     *
+     * @var string[]
+     */
+    protected $visited;
+
+    /**
+     * Constructor
+     *
+     * Note: The last three parameters are only intended to be used when resuming
+     * from a saved state created during a failed harvest.
+     *
+     * @param string $host    Hostname within which to restrict fetching
+     * @param string $outFile Output file for triples
+     * @param array  $queue   Queue (default = empty)
+     * @param array  $visited Visited URLs (default = empty)
+     * @param string $mode    File write mode (default = 'w')
+     */
+    public function __construct($host, $outFile, $queue = [], $visited = [], $mode = 'w')
     {
         $this->host = $host;
-        $this->handle = $handle;
+        $this->handle = fopen($outFile, $mode);
+        $this->outFile = $outFile;
+        if (!$this->handle) {
+            throw new \Exception('Cannot open ' . $outFile . ' for writing.');
+        }
+        $this->queue = $queue;
+        $this->visited = $visited;
     }
 
+    /**
+     * Add a URL to the queue.
+     *
+     * @param string $url URL
+     *
+     * @return void
+     */
     public function enqueue($uri)
     {
         // strip off fragment identifiers:
@@ -67,6 +146,11 @@ class Crawler
         }
     }
 
+    /**
+     * Run the harvest.
+     *
+     * @return void
+     */
     public function run()
     {
         while (!empty($this->queue)) {
@@ -74,6 +158,47 @@ class Crawler
         }
     }
 
+    /**
+     * Save the current state of the crawler to disk.
+     *
+     * @param string $filename Output file name.
+     *
+     * @return void
+     */
+    public function saveState($filename)
+    {
+        $state = [
+            'host' => $this->host,
+            'outFile' => $this->outFile,
+            'queue' => $this->queue,
+            'visited' => $this->visited,
+        ];
+        file_put_contents($filename, serialize($state));
+    }
+
+    /**
+     * Create a new Crawler from a saved state file.
+     *
+     * @param string $filename State to load
+     *
+     * @return Crawler
+     */
+    public static function fromState($filename)
+    {
+        $state = unserialize(file_get_contents($filename));
+        return new Crawler(
+            $state['host'], $state['outFile'],
+            $state['queue'], $state['visited'], 'a'
+        );
+    }
+
+    /**
+     * Internal support method: process the contents of a URI.
+     *
+     * @param string $uri URI to process.
+     *
+     * @return void
+     */
     protected function processUri($uri)
     {
         echo "Accessing $uri...\n";
